d58d198b169c04d9d8346325c398dbbce3895353
Julian <julian822@wp.pl>
2023-01-19 21:42:14 +0100

    Dodałem komentarze do maina

--- main.c
+++ main.c
@@ -4,13 +4,13 @@
 #include "funkcje.h"
 
 int main(void) {
-  int n;
+  int n; // deklaracja zmiennej n typu int//
   int m=3;
-  printf("Podaj liczbę n, której silnię chcesz obliczyć\n");
-  scanf("%d", &n);
+  printf("Podaj liczbę n, której silnię chcesz obliczyć\n"); // wyświetlenie w konsoli komunikatu dla użytkownika //
+  scanf("%d", &n); //pobranie od użytkownika wartości n//
 
-  if(n<0) printf("Podaj liczbę większą równą zero");
+  if(n<0) printf("Podaj liczbę większą równą zero"); // sprawdzenie poprawności liczby wpisanej przez użytkownika //
 
-  else printf("Silnia z liczby %d  to %d",n,silnia(n));
+  else printf("Silnia z liczby %d  to %d",n,silnia(n)); // wyświetlenie wyniku//
   return 0;
 }
